886e0d8c1521d82ee7d44f7cfdb9e22b0a9341ee
potatoeszz <boopioz101@gmail.com>
2023-03-26 15:07:22 -0400

    omg slay soul sister trigger

--- bot.py
+++ bot.py
@@ -13,9 +13,20 @@ token = os.environ.get("TOKEN")
 # create bot client
 client = discord.Client(intents=intents)
 
-def on_ready():
+@client.event
+async def on_ready():
     print(f"{client.user.name} is ready to rock and roll!")
 
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if message.content.startswith("~"):
+        cleanMsg = message.content[1:]
+        await message.channel.send("omg slay soul sister " + " :smirk_cat:" + ":nail_care:")
+    if message.content.startswitch
+
+# log in
 client.run(token)
 
 
