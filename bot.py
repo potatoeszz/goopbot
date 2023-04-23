79eed6763d36b9a76948729d50da6155cf5d74dd
potatoeszz <boopioz101@gmail.com>
2023-04-23 13:38:23 -0400

    add conservation expiration date check

--- bot.py
+++ bot.py
@@ -1,10 +1,28 @@
 import discord
 from dotenv import load_dotenv
 import os
+from math import * 
+import random
+##testing stuff
+from discord.ext import commands
+load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 
+
+
+#variable stuff
+magic_list = ["yes ma'am", "no sir", "maybe", "dunno", "what the heck did you just say", "so so who knows", "bro your stupid no", "bruh obviously"]
 #create intents
 intents = discord.Intents.default()
 intents.message_content = True
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+
+
 
 #read bot token from .env file
 load_dotenv("TOKEN")
@@ -24,7 +42,20 @@ async def on_message(message):
     if message.content.startswith("~"):
         cleanMsg = message.content[1:]
         await message.channel.send("omg slay soul sister " + " :smirk_cat:" + ":nail_care:")
-    if message.content.startswitch
+    if message.content.startswith("math "):
+        cleanMsg = message.content[4:]
+        await message.channel.send("The answer is '" + str(eval(cleanMsg)) + "' you little bozo")
+    if message.content.startswith("nails"):
+        cleanMsg = message.content[5:]
+        await message.channel.send("perioddttt" + " :nail_care:")
+    elif message.content.startswith("8ball"):
+        cleanMsg = message.content[5:]
+        await message.channel.send(str(random.choice(magic_list)))
+    
+        
+#@bot.command("/")
+#async def foo(ctx, arg):
+#    await ctx.send(arg)
 
 # log in
 client.run(token)
---
+++ bot_cmd.py
@@ -0,0 +1,64 @@
+import discord
+from dotenv import load_dotenv
+import os
+from math import *
+from discord.ext import commands
+import random
+import openai
+
+#variables
+messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "You are a middle school teacher."},
+        {"role": "assistant", "content": "Sure. I would like!"},
+    ]
+
+magic_list = ["yes ma'am", "no sir", "maybe", "dunno", "what the heck did you just say", "so so who knows", "bro your stupid no", "bruh obviously"]
+
+#chat gpt
+load_dotenv("C_TOKEN")
+c_token = os.getenv("C_TOKEN")
+
+openai.api_key = c_token
+
+def chat(inp):
+  messages.append({"role": "user", "content": inp})
+  response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=messages
+  )
+  messages.append(response.choices[0].message)
+  return (response.choices[0].message.content)
+
+#create intents
+intents = discord.Intents.default()
+intents.message_content = True
+
+# read bot token from .env file
+load_dotenv("TOKEN")
+token = os.environ.get("TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='-', intents=intents)
+
+@bot.command(help="makes it say some stuff")
+async def test(ctx):
+    await ctx.send("omg slay soul sister " + " :smirk_cat:" + ":nail_care:")
+
+@bot.command(help="repeats what you said")
+async def echo(ctx, *, arg):
+    await ctx.send(arg)
+
+@bot.command(help="does math for you")
+async def math(ctx, *, arg):
+    await ctx.send("the answer is " + str(eval(arg)) + " you little bozo")
+
+@bot.command(help="chat gpt for yall nerds")
+async def ai(ctx, *, arg):
+    await ctx.send(chat(arg))
+
+
+bot.run(token)
+
